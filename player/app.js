967040a2416955910a89862af25efc70672f6445
Adam Gullerud Haeger <adam.haeger@bouvet.no>
2014-10-25 17:34:55 +0200

    change

--- player/app.js
+++ player/app.js
@@ -13,13 +13,13 @@ app.run(function () {
 
 });
 
-app.controller('chatController', ['$scope', '$interval', function($scope, $interval) {
+app.controller('chatController', ['$scope', '$interval', '$location', function($scope, $interval, $location) {
 
 
     var hostUrl = $location.$$protocol+'://'+$location.$$host+':'+$location.$$port;
 
     var player = io.connect(hostUrl);
-    
+
 
 
     player.on('connect', function () {
