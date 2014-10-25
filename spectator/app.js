52ba16c34d7486ff62469287c1c7283ed61cd9de
Terje Wallberg Matsen <terje.matsen@terje.local>
2014-10-25 21:25:38 +0200

    fixes

--- spectator/app.js
+++ spectator/app.js
@@ -21,36 +21,42 @@ app.controller('spectatorController', ['$scope','$location', function($scope,$lo
 	spectator = this;
 	//var socket = io();
 	var socket = io.connect(hostUrl+'/spectator');
-	var spectator
 	var map;
 	var myLatlng = new google.maps.LatLng(59.438006,10.593910);
-	var carebearImg = 'images/carebear.png';
+	//var carebearImg = 'images/carebear.png';
+	var carebearImg = 'http://www.agence2presse.eu/Agence2Presse/Scripts/Widgets/Timer/flip/';
 	var players = [];
 	var playerExists = function(id) {
 		return (typeof players[id] != 'undefined');
 	}
 	var createPlayer = function(id,lat,long) {
 		var playersLatlng = new google.maps.LatLng(lat,long);
+		 
 		players[id] = {
-			id:id,
-			lat:lat,
+			id: id,
+			lat: lat,
 			long:long,
 			marker: new google.maps.Marker({
 			    position: playersLatlng,
 			    map: map,
 			    title:"PLayerId:"+id,
-			    icon: carebearImg
+			    icon: carebearImg+id+'.png'
 			}),
-			//infoWindow: new google.maps.InfoWindow({
-      		//	content: "Player: "+id
-			//});
+			infoWindow: new google.maps.InfoWindow({
+      			content: getInfoWindowContent(id,lat,long),
+		         disableAutoPan: false,		
+     			maxWidth: 150
+			})
 		}
 
+		players[id].infoWindow.open(map,players[id].marker);
+		
 	}
 	var positionPlayer = function(id,lat,long) {
 
 		var playersLatlng = new google.maps.LatLng(lat,long);
 		players[id].marker.setPosition(playersLatlng);
+		//players[id].infoWindow.setContent(getInfoWindowContent(id,lat,long));
 		//players[id].marker.setAnimation(google.maps.Animation.BOUNCE);
 		//setTimeout(function() {players[id].marker.setAnimation(null);}, 2000);
 
@@ -89,7 +95,7 @@ app.controller('spectatorController', ['$scope','$location', function($scope,$lo
 		console.log("Player exists:"+playerExists(1));
 		setTimeout(function() {
 			console.log("Positioning player")
-			positionPlayer(1,59.438006,10.5930000);
+			positionPlayer(1,59.435100,10.5926001);
 		}, 1000);
 		socket.emit('latLong', {
 			id: 3,
@@ -102,14 +108,24 @@ app.controller('spectatorController', ['$scope','$location', function($scope,$lo
 			socket.emit('latLong', {
 				id: 3,
 				lat: 59.439006,
-				long: 10.5939000
+				long: 10.593900
 
 			});
 		}, 3000);
 		
 	}
-	
+	$scope.message = 'hello';
+	function getInfoWindowContent(id,lat,long) {
+		return '<div class="infoWindowContent" id="markerInfo'+id+'">'+
+		      '<div id="siteNotice">'+
+		      '</div>'+
+		      '<h3>Spiller: '+id+'</h3>'+
+		      '<div class="message"></div>'+
+		      //'<p class="lat">lat: '+lat+'</p>'+
+		      //'<p class="lang">long: '+long+'</p>'
+		      '</div>';
+	}
 	spectator.initialize();
-	//test();
+	test();
 
 }]);
